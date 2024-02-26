29d9727c3d1816b322ad1a6770ab5b762bbc53de
Fangeline <fangelineap@gmail.com>
2024-02-26 19:17:25 +0700

    modify js files

--- JavaScript 2/video10-12/script.js
+++ JavaScript 2/video10-12/script.js
@@ -1,36 +1,36 @@
-// const closeBtn = document.querySelector('.close');
-// const card = document.querySelector('.card');
+const closeBtn = document.querySelector('.close');
+const card = document.querySelector('.card');
 
-// closeBtn.addEventListener('click', function () {
-//     card.style.display = 'none';
-// });
+closeBtn.addEventListener('click', function () {
+    card.style.display = 'none';
+});
 
 // cara 1 - without DOM Traversal
-// const close = document.querySelectorAll('.close');
+const close = document.querySelectorAll('.close');
 // const card = document.querySelectorAll('.card');
 
-// for(let i = 0; i < close.length; i++) {
-//     close[i].addEventListener('click', function () {
-//         card[i].style.display = 'none';
-//     });
-// }
+for(let i = 0; i < close.length; i++) {
+    close[i].addEventListener('click', function () {
+        card[i].style.display = 'none';
+    });
+}
 
 
 // cara 2 - with DOM Traversal
-const close = document.querySelectorAll('.close');
+// const close = document.querySelectorAll('.close');
 
-// for(let i = 0; i < close.length; i++) {
-//     close[i].addEventListener('click', function (e) {
-//         // close[i].parentElement.style.display = 'none';
-//         e.target.parentElement.style.display = 'none';
-//     });
-// }
+for(let i = 0; i < close.length; i++) {
+    close[i].addEventListener('click', function (e) {
+        // close[i].parentElement.style.display = 'none';
+        e.target.parentElement.style.display = 'none';
+    });
+}
 
-// close.forEach(element => {
-//     element.addEventListener('click', function(e) {
-//         e.target.parentElement.style.display = 'none';
-//     })
-// });
+close.forEach(element => {
+    element.addEventListener('click', function(e) {
+        e.target.parentElement.style.display = 'none';
+    })
+});
 
 
 
@@ -50,30 +50,30 @@ console.log(nama.previousElementSibling);
 
 /* PREVENT DEFAULT -> method untuk mencegah terjadinya aksi default yang 
 dilakukan oleh elemen dalam web */
-// close.forEach(element => {
-//     element.addEventListener('click', function (e) {
-//         e.target.parentElement.style.display = 'none';
-//         e.preventDefault();
-//         e.stopPropagation();
-//     });
-// });
+close.forEach(element => {
+    element.addEventListener('click', function (e) {
+        e.target.parentElement.style.display = 'none';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+});
 
 
-// /* EVENT BUBBLING -> event yang dimiliki oleh parent akan dijalankan
-// duluan daripada event pada child, untuk stop ini kita dapat menggunakan
-// method .stopPropagation() */
-// const cards = document.querySelectorAll('.card');
-// cards.forEach(function(card) {
-//     card.addEventListener('click', function() {
-//         alert('ok');
-//     });
-// })
+/* EVENT BUBBLING -> event yang dimiliki oleh parent akan dijalankan
+duluan daripada event pada child, untuk stop ini kita dapat menggunakan
+method .stopPropagation() */
+const cards = document.querySelectorAll('.card');
+cards.forEach(function(card) {
+    card.addEventListener('click', function() {
+        alert('ok');
+    });
+})
 
 
-// const container = document.querySelector('.container');
-// container.addEventListener('click', function (e) {
-//     if(e.target.className == 'close') {
-//         e.target.parentElement.style.display = 'none';
-//         e.preventDefault();
-//     }
-// });
+const container = document.querySelector('.container');
+container.addEventListener('click', function (e) {
+    if(e.target.className == 'close') {
+        e.target.parentElement.style.display = 'none';
+        e.preventDefault();
+    }
+});
